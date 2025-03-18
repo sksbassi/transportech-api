c7cc9740d1493a04e50002f1604c257427db3617
SteffanyBassi <sksbassi@minha.fag.edu.br>
2025-03-18 19:26:15 -0300

    1803

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,18 @@
-export class CreateUserDto {}
+import { IsBoolean, IsString } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  idUser: string;
+  @IsString()
+  Document: string;
+  @IsString()
+  Password: string;
+  @IsString()
+  Name: string;
+  @IsString()
+  Phone: string;
+  @IsString()
+  Email: string;
+  @IsBoolean()
+  Status: boolean;
+}
