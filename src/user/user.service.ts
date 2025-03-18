96204533fdf5d4c863fc4916bfc292ae96e1ce4b
SteffanyBassi <sksbassi@minha.fag.edu.br>
2025-03-18 20:11:51 -0300

    1803

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,17 +2,31 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UserService {
   constructor(private prisma: PrismaService) {}
 
+  private mapToEndity(user: any): User {
+    return {
+      idUser: user.idUser,
+      Document: user.Document,
+      Password: user.Password,
+      Name: user.Name,
+      Phone: user.Phone,
+      Email: user.Email,
+      Status: user.Status,
+    };
+  }
+
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
 
-  findAll() {
-    return `This action returns all`;
+  async findAll(): Promise<User[]> {
+    const user = await this.prisma.user.findMany();
+    return user.map((user) => this.mapToEndity(user));
   }
 
   findOne(id: number) {
